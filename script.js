840b38aa90edf514c9e8beda51629adf805a2ef7
riaanand25 <riyaanand596@gmail.com>
2025-02-04 13:08:11 +0530

    updating

--- script.js
+++ script.js
@@ -1,76 +1,68 @@
 import { PokemonData } from "./data.js";
-// console.log(PokemonData);
 
 const PokemonContainer = document.querySelector(".container");
 const SelectBox = document.querySelector("#SelectBox");
-
 async function FetchingPokemon(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
     return {
       image: data.sprites.other.dream_world.front_default,
-      types: data.types.map(typeinfo => typeinfo.type.name) 
+      types: data.types.map(typeinfo => typeinfo.type.name),
     };
-    
   } catch (error) {
     console.log(error);
-
   }
 }
 
+async function fetchingTypes() {
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/type/?limit=21");
+    const data = await response.json();
+    
+    data.results.forEach((type) => {
+      const option = document.createElement("option");
+      option.value = type.name;
+      option.innerText = type.name;
+      SelectBox.appendChild(option);
+    });
+  } catch (error) {
+    // console.log(error);
+  }
+}
+fetchingTypes();
+let allPokemon = [];
+
 async function PokemonDisplay() {
   for (const user of PokemonData) {
+    const { image, types } = await FetchingPokemon(user.url);
+    
     const div = document.createElement("div");
     div.classList.add("ContainPokemonData");
 
     const img = document.createElement("img");
     img.classList.add("PokemonImage");
-    const {image,types}= await FetchingPokemon(user.url);
-    img.src = image
+    img.src = image;
 
     const h1 = document.createElement("h1");
     h1.classList.add("PokemonName");
-    h1.innerHTML = user.name;
+    h1.innerText = user.name;
 
+    const typeText = document.createElement("p");
+    typeText.classList.add("PokemonType");
+    typeText.innerText = `${types.join(", ")}`;
 
-
-   const fetchingType = fetch("https://pokeapi.co/api/v2/type/?limit=21")
-    .then(response => response.json())
-    .then((pokemonTypeData) => {
-      pokemonTypeData.results.forEach((type) => {
-        console.log(type.name);
-      });
-    })
-    console.log(fetchingType);
-  
-
-   
-    
-   
-
-    const p = document.createElement("p");
-    p.textContent =`${types.join(",")}`;
-
-
-    types.forEach(type => {
-      const option = document.createElement("option");
-      option.value = user.name;
-      option.innerHTML = type;
-      SelectBox.appendChild(option);
-  });
-
-
-
-   
-   
-
-    
-
-    div.append(img, h1,p);
+    div.append(img, h1, typeText);
     PokemonContainer.append(div);
+
+    allPokemon.push(div); 
   }
 }
 
 PokemonDisplay();
+
+SelectBox.addEventListener("click",()=>{
+  if(SelectBox.includes(allPokemon)){
+
+  }
+})
